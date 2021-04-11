6cd2de924c02b2dd292569a21cead63912758417
Crypto <73986063+Crypto195@users.noreply.github.com>
2021-04-11 08:39:48 +0530

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -1,4 +1,5 @@
 const { createCanvas, loadImage } = require("canvas");
+const canvacord = require("canvacord");
 const Discord = require("discord.js");
 const SQLite = require("better-sqlite3");
 const sql = new SQLite("./mainDB.sqlite");
@@ -49,72 +50,22 @@ module.exports = {
       return message.reply(
         `**Missing Permission**: ATTACH_FILES or MESSAGE ATTACHMENTS`
       );
-    let replies = [
-      "https://cdn.glitch.com/9ff282de-f1fa-4b29-9ec0-250649e77831%2F20210110_140539.png?v=1610270301563",
-      "https://cdn.glitch.com/9ff282de-f1fa-4b29-9ec0-250649e77831%2F20210110_140606.jpg?v=1610270361828",
-      "https://cdn.glitch.com/9ff282de-f1fa-4b29-9ec0-250649e77831%2F20210110_140633.png?v=1610270399232"
-    ];
 
     let result = Math.floor(Math.random() * replies.length);
     const canvas = createCanvas(1000, 333);
-    const ctx = canvas.getContext("2d");
     const background = await loadImage(`${replies[result]}`);
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-    ctx.beginPath();
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = "#A3A3A3";
-    ctx.globalAlpha = 0.2;
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(180, 216, 775, 65);
-    ctx.fill();
-    ctx.globalAlpha = 1;
-    ctx.strokeRect(180, 216, 775, 65);
-    ctx.stroke();
-
-    ctx.fillStyle = "#838383";
-    ctx.globalAlpha = 0.6;
-    ctx.fillRect(
-      200,
-      216,
-      (100 / (score.level * 2 * 250 + 250)) * score.xp * 7.5,
-      65
-    );
-    ctx.fill();
-    ctx.globalAlpha = 1;
-
-    ctx.font = "30px Arial";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "#beb1b1";
-    ctx.fillText(`${xpInfo} / ${nextXP} XP`, 600, 260);
-
-    ctx.font = "35px Sans";
-    ctx.textAlign = "left";
-    ctx.fillText(user.user.tag, 325, 125);
-
-    ctx.font = "40px Sans";
-    ctx.fillText("Level: ", 350, 170);
-    ctx.fillText(levelInfo, 500, 170);
-
-    ctx.font = "40px Sans";
-    ctx.fillText("Rank: ", 700, 170);
-    ctx.fillText(ranking, 830, 170);
-
-    ctx.arc(170, 160, 120, 0, Math.PI * 2, true);
-    ctx.lineWidth = 6;
-    ctx.strokeStyle = "#A3A3A3";
-    ctx.stroke();
-    ctx.closePath();
-    ctx.clip();
-    const avatar = await loadImage(
-      user.user.displayAvatarURL({ format: "jpg" })
-    );
-    ctx.drawImage(avatar, 40, 40, 250, 250);
-
-    const attachments = new Discord.MessageAttachment(
-      canvas.toBuffer(),
-      "20210110_121139.png"
-    );
-    message.channel.send(attachments);
+    const card = new canvacord.Rank()
+    .setUsername(user.username)
+    .setDiscriminator(user.discriminator)
+    .setRank(rank)
+    .setLevel(levelInfo)
+    .setCurrentXP(xpInfo)
+    .setRequiredXP(nextXP)
+    .setStatus(user.presence.status)
+    .setAvatar(user.displayAvatarURL({ format: "png", size: 1024 }));
+    
+    const img = await card.build();
+  
+  return message.channel.send(new MessageAttachment(img, "rank.png"));
   }
 };
